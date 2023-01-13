a02b70d760bf99aa72a62a7136d09d944751aee6
Mikhail Babaychev <109237708+MikiHousse@users.noreply.github.com>
2023-01-13 19:22:09 +0300

    Create

--- src/components/offer-card/OfferCard.jsx
+++ src/components/offer-card/OfferCard.jsx
@@ -7,25 +7,33 @@ import {OfferCardTypes} from '../../../prop-types/offer-card'
 const STAR_WIDTH = 20;
 
 const OfferCard = (props) => {
-    const {mark, href, img, price, attribute, hasBookmark, rating, name, type} = item;
+    const {item, onMouseEnter, onMouseLeave} = props;
+    const {id, isPremium, previewImage, price, isFavorite, rating, title, type} = item;
     const activeBookmarkClass = hasBookmark ? `place-card__bookmark-button--active` : ``;
+
+    const handleMouseEnter = () => {
+      onMouseEnter(item);
+    }
+
     return (
-        <article className="cities__place-card place-card">
-            {mark && (
+        <article className="cities__place-card place-card"
+          onMouseEnter={handleMouseEnter}
+          onMouseLeave={() => onMouseLeave()}>
+            {isPremium && (
             <div className="place-card__mark">
-                <span>{mark}</span>
+                <span>Premium</span>
             </div>
             )}
         <div className="cities__image-wrapper place-card__image-wrapper">
-          <a href={href}>
-            <img className="place-card__image" src={img} width="260" height="200" alt="Place image"/>
+          <a href='#'>
+            <img className="place-card__image" src={previewImage} width="260" height="200" alt="Place image"/>
           </a>
         </div>
         <div className="place-card__info">
           <div className="place-card__price-wrapper">
             <div className="place-card__price">
-              <b className="place-card__price-value">{price}</b>
-              <span className="place-card__price-text">{`/ ${attribute}`}</span>
+              <b className="place-card__price-value">{`€${price}`}</b>
+              <span className="place-card__price-text">/ night</span>
             </div>
             <button className={`place-card__bookmark-button ${activeBookmarkClass} button`} type="button">
               <svg className="place-card__bookmark-icon" width="18" height="19">
@@ -41,7 +49,9 @@ const OfferCard = (props) => {
             </div>
           </div>
           <h2 className="place-card__name">
-            <a href={href}>{name}</a>
+            <Link to={`offer/${id}`}>
+              {title}
+            </Link>
           </h2>
           <p className="place-card__type">{type}</p>
         </div>
@@ -49,19 +59,19 @@ const OfferCard = (props) => {
     );
 };
 
+OfferCard.defaultProps = {
+  item: {},
+  onMouseEnter: () => undefined,
+  onMouseLeave: () => undefined,
+};
+
 OfferCard.propTypes = {
-    item: PropTypes.shape({
-        id: PropTypes.string.isRequired,
-        mark: PropTypes.string,
-        href: PropTypes.string.isRequired,
-        img: PropTypes.string.isRequired,
-        price: PropTypes.string.isRequired,
-        attribute: PropTypes.string.isRequired,
-        hasBookmark: PropTypes.bool,
-        rating: PropTypes.number.isRequired,
-        name: PropTypes.string.isRequired,
-        type: PropTypes.string.isRequired,
-    }).isRequired
+    /** Данные карточки */
+    item: OfferCardTypes.isRequired,
+    /** Обработчик мыши при наведении наэлемент */
+    onMouseEnter: PropTypes.func.isRequired,
+    /** Обработчик мыши при уходе с элемента */
+    onMouseLeave: PropTypes.func.isRequired,
 };
 
 export default OfferCard;
